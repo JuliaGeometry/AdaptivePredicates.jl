92fac51c8c7dafde661969680b1474463740e3b7
David Gleich <dgleich@purdue.edu>
2024-05-16 12:42:07 -0400

    Remove dep on StaticArrays

--- src/utils.jl
+++ src/utils.jl
@@ -90,75 +90,81 @@ end
 	will be also.)  Does NOT maintain the nonoverlapping or nonadjacent
 	properties.                  
 """
-@inline function fast_expansion_sum_zeroelim(elen::Int, e, flen::Int, f, h)::Int
+@inline function fast_expansion_sum_zeroelim(elen::Int, e, flen::Int, f, ::Val{N})::Tuple{NTuple,Int} where {N}
+  h = ntuple(i -> zero(typeof(e[1])), Val(N))
   @inbounds begin
-  enow = e[1]
-  fnow = f[1]
-  eindex = findex = 1
-  if ((fnow > enow) == (fnow > -enow))
-    Q = enow
-    enow = e[eindex += 1]
-  else
-    Q = fnow
-    fnow = f[findex += 1]
-  end
-  hindex = 0
-  if ((eindex < elen) && (findex < flen)) # still < since pre-increment
+    enow = e[1]
+    fnow = f[1]
+    eindex = findex = 1
     if ((fnow > enow) == (fnow > -enow))
-      Qnew, hh = Fast_Two_Sum(enow, Q)
+      Q = enow
       enow = e[eindex += 1]
     else
-      Qnew, hh = Fast_Two_Sum(fnow, Q)
+      Q = fnow
       fnow = f[findex += 1]
-	end
-    Q = Qnew
-    if hh != 0.0
-      h[hindex+=1] = hh
-	end
-
-    while (eindex < elen) && (findex < flen)  # still < since pre-increment
-      if (fnow > enow) == (fnow > -enow) 
-        Qnew, hh = Two_Sum(Q, enow)
+    end
+    hindex = 0
+    if ((eindex < elen) && (findex < flen)) # still < since pre-increment
+      if ((fnow > enow) == (fnow > -enow))
+        Qnew, hh = Fast_Two_Sum(enow, Q)
         enow = e[eindex += 1]
-      else 
-        Qnew, hh = Two_Sum(Q, fnow)
+      else
+        Qnew, hh = Fast_Two_Sum(fnow, Q)
         fnow = f[findex += 1]
-	  end
+      end
       Q = Qnew
       if hh != 0.0
-        h[hindex += 1] = hh
-	  end
-	end
-  end
+        #h[hindex+=1] = hh
+        h = Base.setindex(h, hh, hindex+=1)
+      end
 
-  while eindex <= elen
-    Qnew, hh = Two_Sum(Q, enow)
-    eindex += 1
-    # We need an extra iteration to calculate Q
-    # but we don't want to access e
-    if eindex <= elen
-        enow = e[eindex]
+      while (eindex < elen) && (findex < flen)  # still < since pre-increment
+        if (fnow > enow) == (fnow > -enow) 
+          Qnew, hh = Two_Sum(Q, enow)
+          enow = e[eindex += 1]
+        else 
+          Qnew, hh = Two_Sum(Q, fnow)
+          fnow = f[findex += 1]
+        end
+        Q = Qnew
+        if hh != 0.0
+          #h[hindex += 1] = hh
+          h = Base.setindex(h, hh, hindex+=1)
+        end
+      end
     end
-    Q = Qnew
-    if hh != 0.0
-      h[hindex += 1] = hh
-	end
-  end
 
-  while findex <= flen
-    Qnew, hh = Two_Sum(Q, fnow)
-    findex += 1
-    if findex <= flen
-        fnow = f[findex]
+    while eindex <= elen
+      Qnew, hh = Two_Sum(Q, enow)
+      eindex += 1
+      # We need an extra iteration to calculate Q
+      # but we don't want to access e
+      if eindex <= elen
+          enow = e[eindex]
+      end
+      Q = Qnew
+      if hh != 0.0
+        #h[hindex += 1] = hh
+        h = Base.setindex(h, hh, hindex+=1)
+      end
     end
-    Q = Qnew
-    if hh != 0.0
-      h[hindex += 1] = hh
-	end
+
+    while findex <= flen
+      Qnew, hh = Two_Sum(Q, fnow)
+      findex += 1
+      if findex <= flen
+          fnow = f[findex]
+      end
+      Q = Qnew
+      if hh != 0.0
+        #h[hindex += 1] = hh
+        h = Base.setindex(h, hh, hindex+=1)
+      end
+    end
+    if (Q != 0.0) || (hindex == 0)
+      #h[hindex += 1] = Q
+      h = Base.setindex(h, Q, hindex+=1)
+    end
+    return h, hindex
   end
-  if (Q != 0.0) || (hindex == 0)
-    h[hindex += 1] = Q
-  end
-  return hindex
-end
 end
