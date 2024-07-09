4921d187a0f82ecc579ba7741831baaacd61e964
DanielVandH <danj.vandenheuvel@gmail.com>
2024-07-09 09:57:21 +0100

    Was using 141 instead of 142

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A Julia port of "Routines for Arbitrary Precision Floating-point Arithmetic and 
 by Jonathan Richard Shewchuk. http://www.cs.cmu.edu/~quake/robust.html
 
 Please note that these predicates are only assumed to be correct for floating point numbers with binary exponents in the 
-range $[-141, 201]$. In particular:
+range $[-142, 201]$. In particular:
 
 > This article does not address issues of overflow and underflow, so I allow the exponent to be an integer in the range  $[-\infty, \infty]$. (Fortunately, many applications have inputs whose exponents fall within a circumscribed range. The four predicates implemented for this article will not overflow nor underflow if their inputs have exponents in the range $[-142, 201]$ and IEEE 754 double precision arithmetic is used.)
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -84,8 +84,8 @@ end
 end
 
 # The next two should agree
-check_range(x) = iszero(x) || exponent(abs(x)) ∈ -141:201   # Shewchuk's predicates are only guaranteed to be accurate for floats with binary 
-                                                            # exponents in the range [-141, 201]. See Section 2.1 in the paper 
+check_range(x) = iszero(x) || exponent(abs(x)) ∈ -142:201   # Shewchuk's predicates are only guaranteed to be accurate for floats with binary 
+                                                            # exponents in the range [-142, 201]. See Section 2.1 in the paper 
                                                             # Richard Shewchuk, J. Adaptive Precision Floating-Point Arithmetic and Fast Robust Geometric Predicates. Discrete Comput Geom 18(3), 305–363 (1997)
 @check function orient_against_exact(a=complexgen2, b=complexgen2, c=complexgen2)
     assume!(all(check_range, (a.re, a.im, b.re, b.im, c.re, c.im)))
